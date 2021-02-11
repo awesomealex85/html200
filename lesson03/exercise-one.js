ef5173e5e222879c55d05e4619d0573a55cc9759
awesomealex85 <alexec206@gmail.com>
2021-02-10 17:14:00 -0800

    lesson3 challange1 initial commit

---
+++ lesson03/exercise-one.js
@@ -0,0 +1,13 @@
+let menu = ['pupusas','tamales','yuca', 'chicharon', 'platano'];
+
+menu.push('empanada');
+
+console.log(menu);
+
+menu.splice(2, 1);
+
+console.log(menu);
+
+const menuList = menu.join(' ');
+
+console.log(menuList);
